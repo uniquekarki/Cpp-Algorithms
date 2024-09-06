f8f85c7938f97464bfb77c1c3583cc006af41875
uniquekarki <unikarki@gmail.com>
2024-09-06 21:52:48 +0545

    Added doubly linked list

--- algorithms/doubly_linked_list.cpp
+++ algorithms/doubly_linked_list.cpp
@@ -4,11 +4,14 @@ using namespace std;
 class Node{
     public:
         int data;
+        Node* prev;
         Node* next;
     
     Node(int val){
         this->data = val;
+        this->prev = nullptr;
         this->next = nullptr;
+
     };
 };
 
@@ -26,9 +29,82 @@ class DoublyLL{
                 head = newNode;
             }
             else{
+                newNode->next = head;
+                head->prev = newNode;
+                head = newNode;
+            };
+        };
+
+        void insertNodetail(int val){
+            Node* newNode = new Node(val);
+            if(head==nullptr){
+                head = newNode;
+            }
+            else{
                 Node* current = head;
-                current->next = head->next;
-                head = current;
+                while(current->next!= nullptr){
+                    current = current->next;
+                };
+                current->next = newNode;
+                newNode->prev = current;
+            };
+        };
+
+        void insertNodeval(int nodeval, int val){
+            Node* current = head;
+            Node* newNode = new Node(val);
+            while(current->data != nodeval){
+                current = current->next;
+            };
+
+            newNode->next = current->next;
+            newNode->prev = current;
+
+            current->next->prev = newNode;
+            current->next = newNode;
+
+        };
+
+        void deleteNodehead(){
+            if(head == nullptr){
+                cout << "Nothing to delete!!!" << endl;
+            }
+            else{
+                Node* current = head;
+                head = head->next;
+                head->prev = nullptr;
+                delete current;
+            };
+        };
+
+        void deleteNodetail(){
+            if(head == nullptr){
+                cout << "Nothing to delete!!!" << endl;
+            }
+            else{
+                Node* current = head;
+                while(current->next!=nullptr){
+                    current = current->next;
+                };
+                current->prev->next = nullptr;
+                delete current;
+
+            };
+        };
+
+        void deleteNodeval(int val){
+            if(head == nullptr){
+                cout << "The Linked List Is Empty!!!" << endl;
+            }
+            else{
+                Node* current = head;
+
+                while(current->data != val){
+                    current = current->next;
+                };
+                current->prev->next = current->next;
+                current->next->prev = current->prev;
+                delete current;
             };
         };
 
@@ -46,11 +122,59 @@ class DoublyLL{
                 cout << endl;
             };
         };
+
+        void displayDesc(){
+            if(head==nullptr){
+                cout << "Linked List Is Empty!!!" << endl;
+            }
+            else{
+                Node* current = head;
+                while(current->next){
+                    current = current->next;
+                };
+                cout << "Doubly Linked List Backwards: ";
+                while(current != nullptr){
+                    cout << current->data << ", ";
+                    current = current->prev;
+                };
+                cout << endl;
+            };
+        };
 };
 
 int main(){
     DoublyLL dll;
     dll.insertNodehead(9);
     dll.displayLL();
+
+    dll.insertNodehead(8);
+    dll.displayLL();
+
+    dll.insertNodetail(7);
+    dll.displayLL();
+
+    dll.insertNodetail(99);
+    dll.displayLL();
+
+    dll.insertNodetail(88);
+    dll.displayLL();
+
+    dll.insertNodetail(77);
+    dll.displayLL();
+
+    dll.insertNodeval(99,1);
+    dll.displayLL();
+
+    dll.deleteNodehead();
+    dll.displayLL();
+
+    dll.deleteNodetail();
+    dll.displayLL();
+
+    dll.deleteNodeval(1);
+    dll.displayLL();
+
+    dll.displayDesc();
+
     return 0;
 }
