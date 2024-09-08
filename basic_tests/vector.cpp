ab270ddddf26b42ee0e818ffedf0370e9301dd46
uniquekarki <unikarki@gmail.com>
2024-09-08 21:02:21 +0545

    Initial commit for vector

---
+++ basic_tests/vector.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+    vector<int> pt_x0 = {1,2};
+    vector<int> pt_x1 = {5,6};
+
+    cout << "Vector 1: " << pt_x0[0] << ", " << pt_x0[1] << endl;
+    cout << "Vector 2: " << pt_x1[0] << ", " << pt_x1[1] << endl;
+    return 0;
+}
