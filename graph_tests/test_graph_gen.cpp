94d4963999679923857dc6cd284b49b0872e0f46
uniquekarki <unikarki@gmail.com>
2024-09-11 22:18:56 +0545

    Tested the function for greedy search

--- graph_tests/test_graph_gen.cpp
+++ graph_tests/test_graph_gen.cpp
@@ -1,6 +1,7 @@
 // C++ program to demonstrate std::distance()
 #include <iostream>
 #include <vector>
+#include <unordered_set>
 #include "graph.h"
 
 using namespace std;
@@ -17,15 +18,34 @@ int main(){
     vector<vector<int>> graph = create_graph(data, 3);
 
     // Print the medoid
-    cout << "Graph: " << endl;
-    for(int i=0; i < graph.size(); i++){
-        cout << "Out edge for " << i << " : ";
-        for(const auto &out: graph[i]){
-            cout << out << " ";
-        };
-        cout << endl;
-    };
+    // cout << "Graph: " << endl;
+    // for(int i=0; i < graph.size(); i++){
+    //     cout << "Out edge for " << i << " : ";
+    //     for(const auto &out: graph[i]){
+    //         cout << out << " ";
+    //     };
+    //     cout << endl;
+    // };
     
+    // cout << "Index of {4.0, 5.0} = " << get_index(data, {5.0, 6.0});
+    vector<float> s = calc_medoid(data);
+    vector<float> xq = {7.0,7.0};
+    pair<vector<int>, unordered_set<int>> result = greedy_search(data, graph, s, xq, 2, 3);
+
+    // Extract the nearest k elements and the set of visited nodes from the result
+    vector<int> nearest_k_elements = result.first;
+    unordered_set<int> visited_nodes = result.second;
+    cout << "Nearest K elems: \n";
+    for (const auto &elem: nearest_k_elements){
+        cout << elem << " ";
+    };
+
+    cout << "\nVisited Nodes: \n";
+    for (const auto &elem: visited_nodes){
+        cout << elem << " ";
+    };
+
+
     cout << endl;
     return 0;
 }
